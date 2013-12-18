8c3c510c97286fcac5c5157f78e02664e63688ab
pcpa <paulo.cesar.pereira.de.andrade@gmail.com>
2013-12-18 17:56:34 -0200

    Properly handle osgi dependencies and multiple arguments

--- javapackages-tools.sh
+++ javapackages-tools.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-mav_list=
+mvn_list=
+osg_list=
 doc_list=
 opt=$1
-shift
 while read file; do
     case "$file" in
     	*/usr/share/maven-fragments/*)
-	    mav_list="$mav_list $file"	;;
+	    mvn_list="$mvn_list $file"	;;
+	*.jar|*/MANIFEST.MF)
+	    osg_list="$osg_list $file"	;;
     	*/usr/share/javadoc/*)
 	    doc_list="$doc_list $file"	;;
     esac
 done
 
-if [ "x$mav_list" != x ]; then
-    for filter in $@; do
-	echo $mav_list | python /usr/lib/rpm/$filter.$opt
-    done
+if [ "x$mvn_list" != x ]; then
+    echo $mvn_list | sed 's/ /\n/g' | python /usr/lib/rpm/maven.$opt
+fi
+if [ "x$osg_list" != x ]; then
+    echo $osg_list | sed 's/ /\n/g' | python /usr/lib/rpm/osgi.$opt
 fi
 if [ $opt = req -a "x$doc_list" != x ]; then
-    echo $doc_list | sh /usr/lib/rpm/javadoc.$opt
+    echo $doc_list | sed 's/ /\n/g' | sh /usr/lib/rpm/javadoc.$opt
 fi
