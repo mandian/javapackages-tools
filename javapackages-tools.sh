6ea12080af0a288521fc0347835a626746d1fb98
Crispin Boylan <crisb@mandriva.org>
2017-02-25 07:45:48 +0000

    4060 add rpm-pid option required in 4.3.2

--- javapackages-tools.sh
+++ javapackages-tools.sh
@@ -5,6 +5,7 @@ osg_list=
 doc_list=
 opt=$1
 builddir=$2
+rpmpid=$3
 while read file; do
     if [ -z "$RPM_BUILD_ROOT" ]; then export RPM_BUILD_ROOT=$file; fi
     case "$file" in
@@ -22,10 +23,10 @@ while [[ "$RPM_BUILD_ROOT" != *buildroot ]] && [[ "$RPM_BUILD_ROOT" != "/" ]]; d
 done
 
 if [ "x$mvn_list" != x ]; then
-    echo $mvn_list | sed 's/ /\n/g' | python /usr/lib/rpm/maven.$opt --cachedir $builddir
+    echo $mvn_list | sed 's/ /\n/g' | python /usr/lib/rpm/maven.$opt --cachedir $builddir --rpm-pid $rpmpid
 fi
 if [ "x$osg_list" != x ]; then
-    echo $osg_list | sed 's/ /\n/g' | python /usr/lib/rpm/osgi.$opt --cachedir $builddir
+    echo $osg_list | sed 's/ /\n/g' | python /usr/lib/rpm/osgi.$opt --cachedir $builddir --rpm-pid $rpmpid
 fi
 if [ $opt = req -a "x$doc_list" != x ]; then
     echo $doc_list | sed 's/ /\n/g' | python /usr/lib/rpm/javadoc.$opt
